bdd7be342f58d131702f745571d8df1fdab1a570
kapil1502 <kapil.verma.dev1@gmail.com>
2024-07-18 02:41:44 +0530

    Features successfully delivered.

---
+++ core/apis/assignments/principal.py
@@ -0,0 +1,33 @@
+from flask import Blueprint
+from core import db
+from core.apis import decorators
+from core.apis.assignments.schema import AssignmentGradeSchema, AssignmentSchema
+from core.apis.responses import APIResponse
+from core.models.assignments import Assignment
+
+principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
+
+
+@principal_assignments_resources.route('/', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_assignments(p):
+    """Returns list of assignments"""
+    principal_assignments = Assignment.get_assignments_by_principal(p.principal_id)
+    principal_assignments_dump = AssignmentSchema().dump(principal_assignments, many=True)
+    return APIResponse.respond(data=principal_assignments_dump)
+
+@principal_assignments_resources.route('/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """reGrade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
